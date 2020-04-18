0b64b4990c6e7edc65cfb22e59d74069a3bbc2db
frederic <frederic.montariol@gmail.com>
2020-04-18 09:03:40 +0200

    make jdk9 a jigsaw module

---
+++ kotlin-inline-string/src/jdk9Main/java/module-info.java
@@ -0,0 +1,7 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+module kotlin.inline.string {
+    requires kotlin.stdlib;
+}
