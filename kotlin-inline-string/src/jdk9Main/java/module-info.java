665a713afe091e612d0160912f98d5b882b92d1c
frederic <frederic.montariol@gmail.com>
2020-04-19 11:35:34 +0200

    back to jdk8 target

--- kotlin-inline-string/src/jdk9Main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
- */
-
-module kotlin.inline.string {
-    requires kotlin.stdlib;
-}
